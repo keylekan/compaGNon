067577cbff14fd9f713159240cbb5c4181493b8b
Quentin Chatelais <quentin@mymushin.com>
2026-01-25 01:36:44 +0100

    Fix command name for registration mail

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('events:send-registration-reminders')
+Schedule::command('mail:registration-reminders')
     ->dailyAt('18:30')
     ->timezone('Europe/Paris');
